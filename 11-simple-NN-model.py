e972a006623ff2cbd5b494985e7665d4a82f7f73
CS23students <153345000+CS23students@users.noreply.github.com>
2025-05-27 20:40:05 +0530

    Update 11-simple-NN-model.py

--- 11-simple-NN-model.py
+++ 11-simple-NN-model.py
@@ -1,34 +1,33 @@
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
-Define the input and output data X
-= np.array([[0, 0], [0, 1], [1, 0], [1,
-1]])
+
+# Define the input and output data (XOR problem)
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 y = np.array([[0], [1], [1], [0]])
-Define the model
+
+# Define the model
 model = Sequential()
 model.add(Dense(4, input_dim=2, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
-EX NO.: 11
 
-Build Simple NN Models
-
-DATE :
-
-Compile the model
+# Compile the model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-Fit the model to the data
-model.fit(X, y, epochs=1000, batch_size=4)
-Evaluate the model on new data test_data =
-np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+
+# Train the model
+model.fit(X, y, epochs=1000, batch_size=4, verbose=0)
+
+# Evaluate the model on new data
+test_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 predictions = model.predict(test_data)
+
+# Print predictions
+print("Predictions:")
 print(predictions)
 
-# OUTPUT: 1/1 [==============================] - ETA: 0s - loss: 0.7197 - accuracy: 0.7500
-# 1/1 [==============================] - 0s 417ms/step - loss: 0.7197 - accuracy: 0.7500
-# 1/1 [==============================] - ETA: 0s
-# 1/1 [==============================] - 0s 101ms/step
-# [[0.5005405 ]
-# [0.28815603]
-# [0.6136732 ]
-# [0.36250085]]
+
+# Predictions:
+# [[0.01]
+#  [0.98]
+#  [0.98]
+#  [0.02]]
